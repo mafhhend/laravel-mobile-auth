f1cc482a45f7cea5e2fed5aa37589de65dfb1d22
samankhdev <samkh.dev@yahoo.com>
2022-02-10 14:20:26 +0330

    init

--- src/resources/views/layouts/master.blade.php
+++ src/resources/views/layouts/master.blade.php
@@ -5,9 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
-    <link rel="stylesheet" href="{{ asset("packages/mafhhend/laravel-mobile-auth/css/tailwind.css") }}">
+    <link rel="stylesheet" href="{{ asset("css/vendor/LaravelMobileAuth/app.css") }}">
 </head>
-<body class="flex flex-col justify-center items-center bg-slate-50 h-screen">
+<body class="flex flex-col items-center justify-center h-screen bg-slate-50">
     @yield("content")
 </body>
 </html>
